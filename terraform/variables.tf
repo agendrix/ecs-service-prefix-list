1daf6ec1876fcbc8edba43ce4836b76edc152e87
Louis Boudreau <l.boudreau@agendrix.com>
2023-02-10 12:49:53 -0500

    Add sns_topic_to_notify_on_failure variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,3 +8,10 @@ variable "ecs_service" {
   type        = string
 }
 
+variable "sns_topic_to_notify_on_failure" {
+  description = "Arn of the sns topic to notify on lambda invocation failure."
+  type        = string
+  default     = null
+}
+
+
